76f64629a72882f0e181ede80470282f86b92861
morgan <morganstar@gmail.com>
2020-10-13 16:47:45 -0700

    Estate contract initial reimplementation

--- contracts/AlfredEstate.sol
+++ contracts/AlfredEstate.sol
@@ -1,4 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
 // Alfred.Estate - Digital Inheritance Automation
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import "@openzeppelin/contracts/math/SafeMath.sol";
+
+/// @title Digital Inheritance Automation
+/// @author Alfred.Estate
+contract AlfredEstate is AccessControl {
+    // Define access control roles
+    bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
+    bytes32 public constant BENEFICIARY_ROLE = keccak256("BENEFICIARY_ROLE");
+    bytes32 public constant ORACLE_ROLE = keccak256("ORACLE_ROLE");
+    bytes32 public constant GNOSIS_SAFE_ROLE = keccak256("GNOSIS_SAFE_ROLE");
+
+    enum Lifesign { Alive, Uncertain, Dead, PlayingDead }
+
+    /// @notice Estate owner's last known lifesign (0: Alive, 1: Uncertain, 2: Dead, 3: PlayingDead)
+    Lifesign public liveness;
+
+    /// @notice Initialize estate owned by caller
+    constructor() {
+        // Grant the contract deployer the default admin role: it will be able
+        // to grant and revoke any roles
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        // Set the estate owner as alive
+        liveness = Lifesign.Alive;
+    }
+
+    /// @notice Accept ETH deposits
+    /// @dev To avoid exceeding gas limit don't perform any other actions
+    receive() external payable { }
+
+    /// @notice Send ETH from estate
+    /// @param recipient Address to send ETH to
+    /// @param amount How much ETH to send, in Wei
+    function sendEth(address payable recipient, uint256 amount) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        recipient.send(amount);
+    }
+
+    /// @notice Send ERC20 token from estate
+    /// @param recipient Address to send ERC20 token to
+    /// @param token Address of ERC20 token to send
+    /// @param amount How much of token to send, in smallest unit
+    function sendToken(address payable recipient, address token, uint256 amount) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        IERC20(token).transfer(recipient, amount);
+    }
+
+}
--- truffle-config.js
+++ truffle-config.js
@@ -83,7 +83,7 @@ module.exports = {
   compilers: {
     solc: {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      version: "0.7.0",
+      version: "0.7.1",
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
