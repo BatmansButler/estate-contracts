f06de31127c7bd97156affdc5dc9ea71853ba30b
morgan <morganstar@gmail.com>
2020-10-22 19:52:56 -0700

    Handle expected transaction failures. More estate tests.

--- test/endowl_estate.js
+++ test/endowl_estate.js
@@ -1,52 +1,88 @@
 const EndowlEstate = artifacts.require("EndowlEstate");
+let catchRevert = require("./utils/exceptions").catchRevert;
 
 contract("EndowlEstate", async accounts => {
+    let estate;
+    let owner = accounts[0];
+    let nonOwner = accounts[1];
+    // How much ETH to test sending to and from the estate
+    let sendAmount = web3.utils.toBN(web3.utils.toWei("10", "ether"));
+
+    before(async () => {
+        // estate = await EndowlEstate.deployed();
+        estate = await EndowlEstate.new();
+    })
+
     it("should be owned by the first account", async () => {
-        let instance = await EndowlEstate.deployed();
-        let owner = await instance.owner();
-        assert.equal(owner, accounts[0]);
+        assert.equal(owner, await estate.owner());
     })
 
     it("should have an initial 'liveness' value of 0 (ie. 'Alive')", async () => {
-        let instance = await EndowlEstate.deployed();
-        let liveness = await instance.liveness()
-        assert.equal(liveness, 0);
+        assert.equal(await estate.liveness(), 0);
     });
 
-    // How much ETH to test sending to and from the estate
-    let sendAmount = web3.utils.toWei("10", "ether");
-
-    it("should accept direct ETH payments", async () => {
-        let instance = await EndowlEstate.deployed();
-        let estateBalance = await web3.eth.getBalance(instance.address);
-        // Confirm the estate balance is initially zero ETH
-        assert.equal(estateBalance, 0);
-        // Send ETH directly to the estate
-        await instance.send(sendAmount);
-        // Confirm the estate balance increased by the amount sent
-        estateBalance = await web3.eth.getBalance(instance.address);
-        assert.equal(estateBalance, sendAmount);
+    it("should accept direct ETH payments from the owner", async () => {
+        let estateBalanceBefore = web3.utils.toBN(await web3.eth.getBalance(estate.address));
+        await estate.send(sendAmount);
+        let estateBalanceAfter = web3.utils.toBN(await web3.eth.getBalance(estate.address));
+        let expectedEstateBalance = estateBalanceBefore.add(sendAmount);
+        assert.isTrue(expectedEstateBalance.eq(estateBalanceAfter));
     });
 
-    it("should send ETH on behalf of owner", async () => {
-        let instance = await EndowlEstate.deployed();
-        let estateBalance = await web3.eth.getBalance(instance.address);
-        // Confirm the ETH payment from the previous test is present
-        assert.isTrue(web3.utils.toBN(estateBalance).gt(web3.utils.toBN(0)), "Estate balance is greater than zero");
-        assert.equal(estateBalance, sendAmount, "Estate balance is expected amount");
-        // Send ETH to a different recipient than the active user
-        let recipient = accounts[1];
-        let recipientBalanceBefore = await web3.eth.getBalance(recipient);
-        await instance.sendEth(recipient, sendAmount);
-        // Send the whole amount that the estate previously received
-        estateBalance = await web3.eth.getBalance(instance.address);
-        // Confirm the estate balance is zero ETH/
-        assert.equal(estateBalance, 0, "Estate balance is zero ETH after sending");
-        let recipientBalanceAfter = await web3.eth.getBalance(recipient);
-        let expectedRecipientBalance = web3.utils.toBN(sendAmount).add(web3.utils.toBN(recipientBalanceBefore));
-        // Confirm the recipient's balance goes up by sendAmount
-        assert.isTrue(expectedRecipientBalance.eq(web3.utils.toBN(recipientBalanceAfter)), "Recipients final balance increased as expected");
+    it("should accept direct ETH payments from a non-owner", async () => {
+        let estateBalanceBefore = web3.utils.toBN(await web3.eth.getBalance(estate.address));
+        await estate.send(sendAmount, {from: nonOwner});
+        let estateBalanceAfter = web3.utils.toBN(await web3.eth.getBalance(estate.address));
+        let expectedEstateBalance = estateBalanceBefore.add(sendAmount);
+        assert.isTrue(expectedEstateBalance.eq(estateBalanceAfter));
     });
+
+    it("should send ETH when the owner calls sendEth(...)", async () => {
+        // Some balance of ETH should still be on the estate from previous test and is needed for this test
+        let estateBalanceBefore = web3.utils.toBN(await web3.eth.getBalance(estate.address));
+        assert.isTrue(estateBalanceBefore.gte(sendAmount), "Test requires estate balance be at least sendAmount");
+
+        // Set recipient to a different account than the caller so we don't have to calculate for gas fees
+        let recipient = nonOwner;
+        let recipientBalanceBefore = web3.utils.toBN(await web3.eth.getBalance(recipient));
+
+        // Call sendEth from the owner
+        await estate.sendEth(recipient, sendAmount);
+
+        // Confirm the estate balance goes down by the expected amount
+        let estateBalanceAfter = web3.utils.toBN(await web3.eth.getBalance(estate.address));
+        let expectedEstateBalance = estateBalanceBefore.sub(sendAmount);
+        assert.isTrue(expectedEstateBalance.eq(estateBalanceAfter), "Wrong estate balance after sendEth()");
+
+        // Confirm the recipient's balance goes up by the expected amount
+        let recipientBalanceAfter = web3.utils.toBN(await web3.eth.getBalance(recipient));
+        let expectedRecipientBalance = recipientBalanceBefore.add(sendAmount);
+        assert.isTrue(expectedRecipientBalance.eq(recipientBalanceAfter), "Wrong recipient balance after sendEth()");
+    });
+
+    it("should revert the transaction when non-owners call sendEth(...)", async () => {
+        // Some balance of ETH should still be on the estate from previous test and is needed for this test
+        let estateBalanceBefore = web3.utils.toBN(await web3.eth.getBalance(estate.address));
+        assert.isTrue(estateBalanceBefore.gte(sendAmount), "Test requires estate balance be at least sendAmount");
+        // Call sendEth from a non-owner, which should fail with a revert
+        await catchRevert(estate.sendEth(owner, sendAmount, {from: nonOwner}));
+    })
+
+    it("should start playing dead when the owner calls playDead()", async () => {
+        assert.equal(await estate.liveness(), 0, "Test requires estate owner to be alive");
+        await estate.playDead();
+        assert.equal(await estate.liveness(), 3, "Playing dead should produce liveness value of 3");
+    })
+
+    it("should stop playing dead when the owner calls confirmLife()", async () => {
+        assert.equal(await estate.liveness(), 3, "Test requires estate owner to be playing dead (liveness value of 3");
+        await estate.confirmLife();
+        assert.equal(await estate.liveness(), 0, "Confirming life should produce liveness value of 0");
+    })
+
+    it("should revert the transaction when a non-owner calls playDead()", async () => {
+        await catchRevert(estate.playDead({from: nonOwner}));
+    })
 
     // TODO: Test sendToken and many other functions...
 })
--- test/endowl_estate_2.sol
+++ test/endowl_estate_2.sol
@@ -9,23 +9,29 @@ contract TestEndowlEstate2 {
     uint public initialBalance = 1 ether;
     address payable estateAddress;
 
+    /// @dev Necessary to receive payments back to this test contract
     receive() external payable {}
 
+    /// @dev Verify owner of newly deployed estate is the caller
     function testNewEstateOwnedByCaller() public {
         EndowlEstate estate = new EndowlEstate();
         estateAddress = address(estate);
         Assert.equal(estate.owner(), address(this), "Newly deployed estate should be owned by calling account");
     }
 
+    /// @dev Sending ETH directly to estate contract from test contract should succeed
     function testSendingEthToEstate() public {
-        // Check ETH is on the test contract and not on the estate
+        // Check ETH is on the test contract and not on the estate to begin the test
         Assert.equal(estateAddress.balance, 0, "Unexpected estate balance");
         Assert.equal(address(this).balance, initialBalance, "Unexpected testing balance");
+        // Send ETH to the estate
         (bool success, ) = estateAddress.call{value: initialBalance}("");
+        // Verify success
         Assert.isTrue(success, "Sending ETH to estate failed");
         Assert.equal(estateAddress.balance, initialBalance, "Unexpected estate balance after send");
     }
 
+    /// @dev The owner of the estate should be able to call sendEth
     function testSendEth() public {
         // Check ETH is on the estate and not the test contract (from the previous test)
         Assert.equal(estateAddress.balance, initialBalance, "Unexpected estate balance");
